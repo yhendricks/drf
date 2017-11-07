028c30f893729bc4ed6857f8fee10a1957888c46
yhendricks <yhendricks1@gmail.com>
2017-11-07 23:29:39 +0200

    Completed video 2.4

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
 from . import models
 from . import serializers
 
@@ -13,4 +14,25 @@ class RetrieveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.CourseSerializer
 
 
+class ListCreateReview(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
 
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+    def perform_create(self, serializer):
+        course = get_object_or_404(models.Course, pk=self.kwargs.get('course_pk'))
+        serializer.save()
+
+
+class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            course_id=self.kwargs.get('course_pk'),
+            pk=self.kwargs.get('pk')
+        )
