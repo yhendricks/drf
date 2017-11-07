5ca27b992d5f0528b79df3f309ffb2a5d61cca85
yhendricks <yhendricks1@gmail.com>
2017-11-07 22:55:07 +0200

    first commit

---
+++ courses/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from . import models
+from . import serializers
+
+# Create your views here.
+class ListCreateCourse(generics.ListCreateAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
+
+
